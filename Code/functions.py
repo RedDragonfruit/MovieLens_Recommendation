55894b59500c183372d3aab0b0cd61dc19bd977d
Shane Mangold <smangold459@outlook.com>
2021-08-23 16:36:47 -0400

    recfactored baseline model, added some markdown

--- Code/functions.py
+++ Code/functions.py
@@ -1,2 +1,37 @@
-def example():
-    print('This is how it works')
+def get_names(*dfs):
+    """
+    Using the package called varname this funciton is able to convert varible names into 
+    attributes of the df stored in .__dfname__ as strings
+    """
+    from varname import argname2
+    #takes variable name and creates a list of strings
+    names = argname2('*dfs')
+    #gives each df an attribute containing name as string
+    for name, df in zip(names,dfs):
+        df.__dfname__ = name
+        
+def movie_rater(movie_df,num, genre=None):
+    userID = 1000
+    rating_list = []
+    while num > 0:
+        if genre:
+            movie = movie_df[movie_df['genres'].str.contains(genre)].sample(1)
+        else:
+            movie = movie_df.sample(1)
+        print(movie)
+        rating = input('How do you rate this movie on a scale of 1-5, press n if you have not seen :\n')
+        if rating == 'n':
+            continue
+        else:
+            rating_one_movie = {'userId':userID,'movieId':movie['movieId'].values[0],'rating':rating}
+            rating_list.append(rating_one_movie) 
+            num -= 1
+    return rating_list  
+
+def recommended_movies(user_ratings,movie_title_df,n):
+        for idx, rec in enumerate(user_ratings):
+            title = movie_title_df.loc[movie_title_df['movieId'] == int(rec[0])]['title']
+            print('Recommendation # ', idx+1, ': ', title, '\n')
+            n-= 1
+            if n == 0:
+                break
