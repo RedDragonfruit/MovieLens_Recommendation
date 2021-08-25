09847095462608de6db24588744dfafd10596672
Shane Mangold <smangold459@outlook.com>
2021-08-25 15:59:42 -0400

    recfatored most of functions into func.py, waiting on doug to touch up offline file

--- Code/func.py
+++ Code/func.py
@@ -1,15 +1,37 @@
-def get_names(*dfs):
-    """
-    Using the package called varname this funciton is able to convert varible names into 
-    attributes of the df stored in .__dfname__ as strings
-    """
-    from varname import argname2
-    #takes variable name and creates a list of strings
-    names = argname2('*dfs')
-    #gives each df an attribute containing name as string
-    for name, df in zip(names,dfs):
-        df.__dfname__ = name
-        
+import pandas as pd
+import numpy as np
+from sklearn.metrics import pairwise_distances
+from surprise import Reader, Dataset, accuracy, SVDpp
+from surprise.model_selection import cross_validate, GridSearchCV, train_test_split
+from surprise.prediction_algorithms import SVD, KNNWithMeans, KNNBasic, KNNBaseline
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import linear_kernel
+
+movies = pd.read_csv('Data/movies.csv')
+ratings = pd.read_csv('Data/ratings.csv')
+movies_ratings=pd.merge(movies, ratings)
+
+movies_ratings=pd.merge(movies, ratings)
+ratings_matrix_items = movies_ratings.pivot_table(index=['movieId'],columns=['userId'],values='rating').reset_index(drop=True)
+ratings_matrix_items.fillna( 0, inplace = True )
+
+movie_similarity = 1 - pairwise_distances( ratings_matrix_items.values, metric="cosine" )
+np.fill_diagonal( movie_similarity, 0 ) #Filling diagonals with 0s for future use when sorting is done
+ratings_matrix_items = pd.DataFrame( movie_similarity )
+ratings_matrix_items
+
+ratings_matrix_users = movies_ratings.pivot_table(index=['userId'],columns=['movieId'],values='rating').reset_index(drop=True)
+ratings_matrix_users.fillna( 0, inplace = True )
+movie_similarity = 1 - pairwise_distances( ratings_matrix_users.values, metric="cosine" )
+np.fill_diagonal( movie_similarity, 0 ) #Filling diagonals with 0s for future use when sorting is done
+ratings_matrix_users = pd.DataFrame( movie_similarity )
+ratings_matrix_users
+
+similar_user_series= ratings_matrix_users.idxmax(axis=1)
+df_similar_user= similar_user_series.to_frame()
+df_similar_user.columns=['similarUser']
+df_similar_user.head()
+
 def movie_rater(movie_df,num, genre=None):
     userID = 1000
     rating_list = []
@@ -26,7 +48,7 @@ def movie_rater(movie_df,num, genre=None):
             rating_one_movie = {'userId':userID,'movieId':movie['movieId'].values[0],'rating':rating}
             rating_list.append(rating_one_movie) 
             num -= 1
-    return rating_list  
+    return rating_list   
 
 def recommended_movies(user_ratings,movie_title_df,n):
         for idx, rec in enumerate(user_ratings):
@@ -35,3 +57,236 @@ def recommended_movies(user_ratings,movie_title_df,n):
             n-= 1
             if n == 0:
                 break
+                
+#using TF-IDF Vectorizer Oject to calculate recommendations
+def get_recommendations_based_on_genres(movie_title):#, cosine_score=cosine_score):
+    """
+    Calculates top 2 movies to recommend based on given movie titles genres. 
+    :param movie_title: title of movie to be taken for base of recommendation
+    :param cosine_sim_movies: cosine similarity between movies 
+    :return: Titles of movies recommended to user
+    """
+    movies = pd.read_csv('Data/movies.csv')
+    ratings = pd.read_csv('Data/ratings.csv')
+    
+    tfidf_movies_genres = TfidfVectorizer(token_pattern = '[a-zA-Z0-9\-]+')
+    tfidf_movies_genres_matrix = tfidf_movies_genres.fit_transform(movies['genres'])
+    cosine_score = linear_kernel(tfidf_movies_genres_matrix, tfidf_movies_genres_matrix)
+    
+    # Get the index of the movie that matches the title
+    idx_movie = movies.loc[movies['title'].isin([movie_title])]
+    idx_movie = idx_movie.index
+    
+    # Get the pairwsie similarity scores of all movies with that movie
+    sim_scores_movies = list(enumerate(cosine_score[idx_movie][0]))
+    
+    # Sort the movies based on the similarity scores
+    sim_scores_movies = sorted(sim_scores_movies, key=lambda x: x[1], reverse=True)
+
+    # Get the scores of the 10 most similar movies
+    sim_scores_movies = sim_scores_movies[0:4]
+    
+    # Get the movie indices
+    movie_indices = [i[0] for i in sim_scores_movies if i[0]!=idx_movie]
+    
+    # Return the top 2 most similar movies
+    return movies['title'].iloc[movie_indices]
+
+def get_recommendation_content_model(userId):
+    """
+    Calculates top movies to be recommended to user based on movie user has watched.  
+    :param userId: userid of user
+    :return: Titles of movies recommended to user
+    """
+    recommended_movie_list = []
+    movie_list = []
+    df_rating_filtered = ratings[ratings["userId"]== userId]
+    for key, row in df_rating_filtered.iterrows():
+      
+        movie_list.append((movies["title"][row["movieId"]==movies["movieId"]]).values) 
+    for index, movie in enumerate(movie_list):
+        for key, movie_recommended in get_recommendations_based_on_genres(movie[0]).iteritems():
+            recommended_movie_list.append(movie_recommended)
+
+    # removing already watched movie from recommended list    
+    for movie_title in recommended_movie_list:
+        if movie_title in movie_list:
+            recommended_movie_list.remove(movie_title)
+    
+    return set(recommended_movie_list)
+
+
+#function where returns similar movies based on item to item:
+def item_similarity(movieName): 
+    """
+    recomendates similar movies
+   :param data: name of the movie 
+   """
+    try:
+        #user_inp=input('Enter the reference movie title based on which recommendations are to be made: ')
+        user_inp=movieName
+        inp=movies[movies['title']==user_inp].index.tolist()
+        inp=inp[0]
+
+        movies['similarity'] = ratings_matrix_items.iloc[inp]
+        movies.columns = ['movieId', 'title', 'genre','similarity']
+    except:
+        print("Sorry, the movie is not in the database!")
+        
+def recommendedMoviesAsperItemSimilarity(user_id):
+    """
+     Recommending movie which user hasn't watched as per Item Similarity
+    :param user_id: user_id to whom movie needs to be recommended
+    :return: movieIds to user 
+    """
+    
+    try:
+        user_movie= movies_ratings[(movies_ratings.userId==user_id) & movies_ratings.rating.isin(range(4,5))][['title']]
+        user_movie=user_movie.iloc[0,0]
+
+        try:
+        #user_inp=input('Enter the reference movie title based on which recommendations are to be made: ')
+            user_inp=user_movie
+            inp=movies[movies['title']==user_inp].index.tolist()
+            inp=inp[0]
+
+            movies['similarity'] = ratings_matrix_items.iloc[inp]
+            movies.columns = ['movieId', 'title', 'genre','similarity']
+        except:
+            print("Sorry, the movie is not in the database!")
+
+        sorted_movies_as_per_userChoice=movies.sort_values(by=["similarity"], ascending = False )
+        sorted_movies_as_per_userChoice=sorted_movies_as_per_userChoice[sorted_movies_as_per_userChoice['similarity'] >=0.45]['movie_id']
+        recommended_movies=list()
+        df_recommended_item=pd.DataFrame()
+        user2Movies= ratings[ratings['userId']== user_id]['movieId']
+        for movieId in sorted_movies_as_per_userChoice:
+                if movieId not in user2Movies:
+                    d=[]
+
+                    df_new= ratings[(ratings.movieId==movieId)]
+                    df_recommended_item=pd.concat([df_recommended_item,df_new])
+                    best10=df_recommended_item.sort_values(["rating"], ascending = False )[1:25] 
+                    d= set(best10['movieId'].tolist())
+
+
+        return d
+    
+    
+    #recommend based on disliked movies if the selected user does not have a 4 or above rated movie
+    except:
+        print("this user does not have a movie she has rated above 4 to start the recommendation")
+        user_movie= movies_ratings[(movies_ratings.userId==user_id) & movies_ratings.rating.isin(range(0,3))][['title']]
+        user_movie=user_movie.iloc[0,0]
+
+        try:
+        #user_inp=input('Enter the reference movie title based on which recommendations are to be made: ')
+            user_inp=user_movie
+            inp=movies[movies['title']==user_inp].index.tolist()
+            inp=inp[0]
+
+            movies['similarity'] = ratings_matrix_items.iloc[inp]
+            movies.columns = ['movieId', 'title', 'genre','similarity']
+        except:
+            print("Sorry, the movie is not in the database!")
+
+        sorted_movies_as_per_userChoice=movies.sort_values(by=["similarity"], ascending = True )
+        sorted_movies_as_per_userChoice=sorted_movies_as_per_userChoice[sorted_movies_as_per_userChoice['similarity'] < 0.1]['movieId']
+        recommended_movies=list()
+        df_recommended_item=pd.DataFrame()
+        user2Movies= ratings[ratings['userId']== user_id]['movieId']
+        for movieId in sorted_movies_as_per_userChoice:
+                if movieId not in user2Movies:
+                    d=[]
+
+                    df_new= ratings[(ratings.movieId==movieId)]
+                    df_recommended_item=pd.concat([df_recommended_item,df_new])
+                    best10=df_recommended_item.sort_values(["rating"], ascending = False )[1:10] 
+                    d= set(best10['movieId'].tolist())
+
+
+        return d
+
+def movieIdToTitle(listMovieIDs):
+    """
+     Converting movieId to titles
+    :param user_id: List of movies
+    :return: movie titles
+    """
+    movie_titles= list()
+    for id in listMovieIDs:
+        movie_titles.append(movies[movies['movieId']==id]['title'])
+    return movie_titles
+
+#implementing multiple recommendaiton systems together
+def hybrid_model(user_id= None, new_user=False):
+    
+    """user_id: The id of the user that you would like to make recommendations for
+    
+       new_user: if you are a new user input True for this parameter.
+                 Our function will then ask you for a genre and give you movies from that genre for you to rate.
+                 This addresses the cold start problem
+    """
+    
+    if new_user:
+        genre= input('name a genre of movies that you like: ')
+        user_rating = movie_rater(df_joined, 4, 'Action')
+        
+        new_ratings = df_new.append(user_rating,ignore_index=True)
+        new_data = Dataset.load_from_df(new_ratings,reader)
+        
+        svd_ = SVD(n_factors=200, n_epochs= 40, reg_all= 0.1)
+        svd_.fit(new_data.build_full_trainset())
+        
+        list_of_movies = []
+        
+        for m_id in df_new['movieId'].unique():
+            list_of_movies.append( (m_id,svd_.predict(100,m_id)[3]))
+        
+        ranked_movies = sorted(list_of_movies, key=lambda x:x[1], reverse=True)
+        
+        print("\n\n")
+        recommended_movies(ranked_movies, movies, 5)
+        
+        
+    else:
+        recommended_movies_by_content_model = get_recommendation_content_model(user_id)
+        recommended_movies_by_content_model = movies[movies.apply(lambda movie: movie["title"] in recommended_movies_by_content_model, axis=1)]
+    
+    
+        for key, col in recommended_movies_by_content_model.iterrows():
+            predict = svd.predict(user_id, col["movieId"])
+            recommended_movies_by_content_model.loc[key, "svd_rating"] = predict.est
+    
+    
+        return recommended_movies_by_content_model.sort_values("svd_rating", ascending=False).iloc[0:11]
+    
+def getRecommendedMoviesAsperUserSimilarity(userId):
+    """
+     Recommending movies which user hasn't watched as per User Similarity
+    :param user_id: user_id to whom movie needs to be recommended
+    :return: movieIds to user 
+    """
+    user2Movies= ratings[ratings['userId']== userId]['movieId']
+    sim_user=df_similar_user.iloc[0,0]
+    df_recommended=pd.DataFrame(columns=['movieId','title','genres','userId','rating','timestamp'])
+    for movieId in ratings[ratings['userId']== sim_user]['movieId']:
+        if movieId not in user2Movies:
+            df_new= movies_ratings[(movies_ratings.userId==sim_user) & (movies_ratings.movieId==movieId)]
+            df_recommended=pd.concat([df_recommended,df_new])
+        best10=df_recommended.sort_values(['rating'], ascending = False )[1:10]  
+    return best10['movieId']
+
+def get_user_similar_movies( user1, user2 ):
+    
+    """
+     Returning common movies and ratings of same for both the users
+    :param user1,user2: user ids of 2 users need to compare
+    :return: movieIds to user 
+    """
+    common_movies = movies_ratings[movies_ratings.userId == user1].merge(
+      movies_ratings[movies_ratings.userId == user2],
+      on = "movieId",
+      how = "inner" )
+    common_movies.drop(['movieId','genres_x','genres_y', 'timestamp_x','timestamp_y','title_y'],axis=1,inplace=True)
+#     return common_movies
